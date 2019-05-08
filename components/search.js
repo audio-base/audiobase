c422ec063cc7048fa04babe65e76c05e970cc403
ohannieho <ohannieho123@yahoo.com>
2019-05-08 11:29:13 -0700

    able to get the state of the search input

--- components/search.js
+++ components/search.js
@@ -1,19 +1,27 @@
-// import React from 'react';
-// import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-// class Search extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       search: ''
-//     };
-//   }
-//   render() {
-//     return (
-//       <View>
-//         <TextInput placeholder="search" />
-//       </View>
-//     );
-//   }
-// }
-// export default Search;
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: ''
+    };
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+  handleSearch(e) {
+    e.preventDefault;
+    axios.get();
+  }
+  render() {
+    return (
+      <View>
+        <TextInput
+          placeholder="search"
+          onChangeText={text => this.setState({ search: text })}
+        />
+      </View>
+    );
+  }
+}
+export default Search;
