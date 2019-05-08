d80db851acd21cb40af6c6350602e81055be4c9a
ohannieho <ohannieho123@yahoo.com>
2019-05-07 21:12:47 -0700

    adding init files
    implemented the nav bar

---
+++ App.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import { createAppContainer } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.navContainer}>
+        <Text>+ create</Text>
+        {/* <TextInput placeholder="search" /> */}
+      </View>
+    );
+  }
+}
+class SearchScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: ''
+    };
+  }
+  render() {
+    return (
+      <View style={styles.navContainer}>
+        <TextInput placeholder="search" />
+      </View>
+    );
+  }
+}
+
+class ChatScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.navContainer}>
+        <Text>let's chat!</Text>
+        {/* <TextInput placeholder="search" /> */}
+      </View>
+    );
+  }
+}
+
+export default createAppContainer(
+  createMaterialBottomTabNavigator(
+    {
+      Home: {
+        screen: HomeScreen,
+        navigationOptions: {
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ tintColor }) => (
+            <Icon name="ios-home" color={tintColor} size={24} />
+          )
+        }
+      },
+
+      Search: {
+        screen: SearchScreen,
+        navigationOptions: {
+          tabBarLabel: 'Search',
+          tabBarIcon: ({ tintColor }) => (
+            <Icon name="ios-search" color={tintColor} size={24} />
+          )
+        }
+      },
+      Chat: {
+        screen: ChatScreen,
+        navigationOptions: {
+          tabBarLabel: 'Chat',
+          tabBarIcon: ({ tintColor }) => (
+            <Icon name="md-chatboxes" color={tintColor} size={24} />
+          )
+        }
+      }
+    },
+    {
+      initialRouteName: 'Home',
+      order: ['Home', 'Search', 'Chat'],
+      // defaultNavigationOptions: {
+      //   headerStyle: {
+      //     backgroundColor: '#fff'
+      //   }
+      // },
+      barStyle: { backgroundColor: '#694fad' }
+    }
+  )
+);
+
+const styles = StyleSheet.create({
+  navContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    // justifyContent: 'center',
+    top: 30,
+    margin: 10
+  }
+});
