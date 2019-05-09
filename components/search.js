57221b57f7fc765cd157b0b1f4a8a68550e7f6b0
ohannieho <ohannieho123@yahoo.com>
2019-05-09 14:48:26 -0700

    enabled scrolling on the search page

--- components/search.js
+++ components/search.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  FlatList,
-  Button
-} from 'react-native';
+import { StyleSheet, Text, TextInput, View, ScrollView } from 'react-native';
 import { List, ListItem, SearchBar } from 'react-native-elements';
 import { SC_KEY, db } from '../config.js';
 import axios from 'axios';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { FlatList } from 'react-native-gesture-handler';
 
 let addItem = (item, title, artwork) => {
   db.ref('/songs').push({
@@ -33,12 +27,13 @@ class Search extends React.Component {
     };
     this.handleSearch = this.handleSearch.bind(this);
     this.addSong = this.addSong.bind(this);
+    // this.renderItem = this.renderItem.bind(this);
   }
   componentDidMount() {
     this.handleSearch();
   }
   handleSearch() {
-    let limit = 10;
+    let limit = 20;
     let page = 0;
     let query = this.state.query;
     axios
@@ -63,10 +58,41 @@ class Search extends React.Component {
       () => addItem(this.state.uri, this.state.title, this.state.artwork)
     );
   }
+  // renderItem(obj) {
+  //   return this.state.data.map((obj, i) => {
+  //     return (
 
+  //       <ListItem
+  //         key={i}
+  //         leftAvatar={{
+  //           source: {
+  //             uri: obj.artwork_url ? obj.artwork_url : obj.user.avatar_url
+  //           }
+  //         }}
+  //         title={obj.title}
+  //         rightElement={
+  //           <Icon
+  //             containerStyle={{ alignSelf: 'flex-start' }}
+  //             type="material"
+  //             color="#C8C8C8"
+  //             name="md-add"
+  //             onPress={() =>
+  //               this.addSong(
+  //                 obj.uri,
+  //                 obj.title,
+  //                 obj.artwork_url ? obj.artwork_url : obj.user.avatar_url
+  //               )
+  //             }
+  //           />
+  //         }
+  //       />
+
+  //     );
+  //   });
+  // }
   render() {
     return (
-      <View style={styles.searchContainer}>
+      <ScrollView style={styles.searchContainer}>
         <SearchBar
           style={styles.searchBar}
           clearTextOnFocus={false}
@@ -107,7 +133,25 @@ class Search extends React.Component {
             </View>
           );
         })}
-      </View>
+      </ScrollView>
+      // <FlatList
+      //   style={styles.searchContainer}
+      //   data={this.state.data}
+      //   renderItem={this.renderItem}
+      //   ListHeaderComponent={
+      //     <SearchBar
+      //       style={styles.searchBar}
+      //       clearTextOnFocus={false}
+      //       placeholder="search"
+      //       autoCorrect={false}
+      //       onChangeText={text => this.setState({ query: text })}
+      //       value={this.state.query}
+      //       onSubmitEditing={this.handleSearch}
+      //       platform="ios"
+      //       // showLoading={true}
+      //     />
+      //   }
+      // />
     );
   }
 }
